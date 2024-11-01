910b6dc6253c80f1210b1413b55df6a3c58b9e48
l0g1ka2000 <153945709+l0g1ka2000@users.noreply.github.com>
2024-11-01 19:29:37 +0200

    Add files via upload

---
+++ QSS.py
@@ -0,0 +1,52 @@
+QSS = '''
+    QWidget {
+        background-color: #7fabfa;  /* Світло-сірий фон */
+        font-family: Arial, sans-serif;  /* Шрифт Arial */
+        font-size: 14px;  /* Розмір шрифту */
+    }
+
+    QLabel {
+        color: #333;  /* Темно-сірий текст для міток */
+        font-weight: bold;  /* Жирний текст */
+    }
+
+    QListWidget {
+        background-color: #7fabfa;  /* Білий фон для списків */
+        border: 2px solid #4578d6;  /* Світло-сірий обвод */
+        padding: 5px;  /* Внутрішній відступ */
+    }
+
+    QPushButton {
+        background-color: #4CAF50;  /* Зелений фон */
+        color: white;  /* Білий текст */
+        border: 2px solid #4578d6;  /* Без обводів */
+        padding: 10px;  /* Внутрішній відступ */
+        
+       
+    }
+
+    QPushButton:hover {
+        background-color: #45a049;  /* Темніший зелений при наведенні */
+    }
+
+    QLineEdit {
+        background-color: #7fabfa   ;  /* Білий фон */
+        border: 2px solid #4578d6;  /* Світло-сірий обвод */
+        padding: 5px;  /* Внутрішній відступ */
+    }
+
+    QTextEdit {
+        background-color: #7fabfa;  /* Білий фон */
+        border: 2px solid #4578d6;  /* Світло-сірий обвод */
+        padding: 5px;  /* Внутрішній відступ */
+    }
+
+    QListWidget:item {
+        padding: 10px;  /* Внутрішній відступ для елементів списку */
+    }
+
+    QListWidget:item:selected {
+        background-color: #4CAF50;  /* Зелений фон для вибраних елементів */
+        color: white;  /* Білий текст для вибраних елементів */
+    }
+'''
